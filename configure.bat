8830d494b4c6f1de5558b36d33ebf34bbe27c54d
Rowell Cruz <rcruz@rim.com>
2012-02-03 15:16:43 -0500

    Set the version of jasmine-node to be a compatible one

--- configure.bat
+++ configure.bat
@@ -1,1 +1,1 @@
-npm install -g jake jshint@0.5.0 csslint uglify-js && npm install jasmine-node && npm install jWorkflow
+npm install -g jake jshint@0.5.0 csslint uglify-js && npm install jasmine-node@1.0.7 && npm install jWorkflow
