f6fc6a488492177b314c3279a5d26c0800ce01d4
Adrian Lee <adrilee@rim.com>
2012-02-07 17:04:36 -0500

    Integrate download & unzip of jsdocs-toolkit in build script #7

--- build/build/docs.js
+++ build/build/docs.js
@@ -1,4 +1,8 @@
 var fs = require('fs'),
+    http = require('http'),
+    url = require('url'),
+    wrench = require('wrench'),
+    zip = require("zip"),
     _c = require('./conf'),
     childProcess = require("child_process"),
     path = require('path'),
@@ -17,6 +21,47 @@ function _done(error) {
     } else {
         util.puts("webplatform-framework DOCS SUCCESS");
         process.exit();
+    }
+}
+
+function _getJSDocToolkit(callback) {
+    var jsdoc_url = url.parse(_c.DEPENDENCIES_JSDOC_URL)
+    
+    http.get({'host': jsdoc_url.host, 'path': jsdoc_url.pathname}, function (res) {
+        var stream = fs.createWriteStream(_c.DEPENDENCIES_JSDOC_ZIP);
+        res.pipe(stream);
+        res.on('end', function() {
+            callback();
+        });
+    }).on('error', function(e) {
+        throw (new Error("JSDocs Unable to Download ..."));
+    });
+}
+
+function _exractJSDocToolkit(callback) {
+    var data, filesObj, p, parent;
+    var from = _c.DEPENDENCIES_JSDOC_ZIP;
+    var to = _c.DEPENDENCIES;
+    exists = path.existsSync(from);
+
+    if (exists) {
+        data = fs.readFileSync(from);
+        filesObj = zip.Reader(data).toObject("utf-8");
+
+        if (!path.existsSync(to)) {
+            wrench.mkdirSyncRecursive(to, "0755");
+        }
+
+        for (p in filesObj) {
+            if (p.split("/").length > 1) {
+                parent = p.split("/").slice(0, -1).join("/");
+                wrench.mkdirSyncRecursive(to + "/" + parent, "0755");
+            }
+
+            fs.writeFileSync(to + "/" + p, filesObj[p]);
+        }
+    } else {
+        throw (new Error("JSDocs .zip is Missing ..."));
     }
 }
 
@@ -39,7 +84,6 @@ function _getJSDocCommand() {
         "-d=" + _c.DOCS;
     
     return jsDocsCommand;
-
 }
 
 module.exports = function (src, baton) {
@@ -47,19 +91,24 @@ module.exports = function (src, baton) {
         baton.take();
     }
 
-    childProcess.exec(_getJSDocCommand(), function (error, stdout, stderr) {
-        if (error) {
-            console.log(stdout);
-            console.log(stderr);
-            if (baton) {
-                baton.pass(error.code);
-            }
-        } else {
-            if (baton) {
-                baton.pass(src);
-            }
-        }
-        _done(error);
+    _getJSDocToolkit(function () {
+        _exractJSDocToolkit(build_docs);
     });
-  
+
+    function build_docs() {
+        childProcess.exec(_getJSDocCommand(), function (error, stdout, stderr) {
+            if (error) {
+                console.log(stdout);
+                console.log(stderr);
+                if (baton) {
+                    baton.pass(error.code);
+                }
+            } else {
+                if (baton) {
+                    baton.pass(src);
+                }
+            }
+            _done(error);
+        });
+    }
 };
